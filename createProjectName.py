881b32bc5c349ee4b18ae86b97d3fed588a33a17
Nexzis <36156471+Nexzis@users.noreply.github.com>
2021-12-07 16:48:21 +0300

    change pickle type

--- createProjectName.py
+++ createProjectName.py
@@ -5,14 +5,12 @@ import datetime
 import subprocess
 import pickle
 
-MY_USER = 'NRS'
+
 now = datetime.datetime.now()
 time = now.strftime("%Y-%m-%d_%H-%M") #Текущая дата и время с использованием strftime:
-#user_backup = ''
-#project_backup = 0
-#sonata_backup = 0
 app = tkinter.Tk()
 app.geometry('400x150')
+app.resizable(False, False)
 app.title("createProjectName")  
 user_lbl = tkinter.Label(width="7",  text='user')
 combo_project = ttk.Combobox(width="15")
@@ -21,73 +19,65 @@ user_ent = Entry(width="7")
 but = Button(text="Gen" , command= lambda:create_project_name( user_ent.get() , combo_project.get() , combo_sonata.get()) )
 res = Entry(width="40")
 
-projects = [
-    "SEV",
-    "KOV_ASU_TP_ESN",
-    "KNG_PHG_VRK",
-    "NOVOUB_ASUE",
-    "NOVOURD_ASUE",
-    "ZPLR_ASU_TP_ESN",
-    "ZPLR_PRU",
-    "ZPLR_SAU_BPG",
-    "SOSN",
-]
 
-sonata = [
-    "r10021",
-    "r10223",
-    "r7861",
-    "r10255",
-    "r10277",
-    "r10301", 
-]
+data_old = {
+    'user': ['NRS'],
+    'projects': [   "SEV",
+                    "KOV_ASU_TP_ESN",
+                    "KNG_PHG_VRK",
+                    "NOVOUB_ASUE",
+                    "NOVOURD_ASUE",
+                    "ZPLR_ASU_TP_ESN",
+                    "ZPLR_PRU",
+                    "ZPLR_SAU_BPG",
+                    "SOSN",],
+    'versions': [   "10021",
+                    "10223",
+                    "7861",
+                    "10255",
+                    "10277",
+                    "10301",
+                    "10335"],
+    'current_settings' : ['test',0,0]
+}
+
+
+def open_data():
+    try:
+        with open('config.pickle', 'rb') as f:
+            data = pickle.load(f)
+    except FileNotFoundError:
+        data = data_old                       
+    return data
+
+def convert_data(user_backup,project_backup,sonata_backup):
+    user_ent.insert( 1, user_backup) 
+    combo_project.current(project_backup)   
+    combo_sonata.current(sonata_backup)    
 
 
 def create_project_name(user, Project_attr, Sonata_attr):
     res.delete(0, END)
-    res.insert(0,  (user + '_' + Project_attr + '_' + time + '_' + Sonata_attr))
-    result = user + '_' + Project_attr + '_' + time + '_' + Sonata_attr
+    res.insert(0,  (user + '_' + Project_attr + '_' + time + '_r' + Sonata_attr))
+    result = user + '_' + Project_attr + '_' + time + '_r' + Sonata_attr
+    print(result)
     prepare_data()
     cmd='echo '+result.strip()+'|clip'
     return subprocess.check_call(cmd, shell=True)
 
+
 def prepare_data():
-    with open('data1.pickle', 'wb') as f:
-        pickle.dump(user_ent.get(), f)
-    with open('data2.pickle', 'wb') as f:
-        pickle.dump(combo_project.get(), f)
-    with open('data3.pickle', 'wb') as f:
-        pickle.dump(combo_sonata.get()  , f)
- 
-def open_data():
-    try:
-        with open('data1.pickle', 'rb') as f:
-            user_backup = pickle.load(f)
-    except FileNotFoundError:
-        user_backup = MY_USER
-    try:
-        with open('data2.pickle', 'rb') as f:
-            project_backup = projects.index(pickle.load(f))
-    except FileNotFoundError:
-        project_backup = 0
-    try:
-        with open('data3.pickle', 'rb') as f:
-            sonata_backup = sonata.index(pickle.load(f))
-    except FileNotFoundError:
-        sonata_backup = 0        
-    return user_backup, project_backup, sonata_backup
-
-def convert_data(user_backup,project_backup,sonata_backup):
-    user_ent.insert( 1, user_backup) 
-    combo_project.current(project_backup)
-    combo_sonata.current(sonata_backup)
+    data['current_settings'][0] = user_ent.get()
+    data['current_settings'][1] = combo_project.current()
+    data['current_settings'][2] = combo_sonata.current()
+    with open('config.pickle', 'wb') as f:
+        pickle.dump(data, f) 
 
 
-combo_project.configure(values = projects)
-combo_sonata.configure(values = sonata)
-user_backup,project_backup,sonata_backup = open_data()
-convert_data(user_backup,project_backup,sonata_backup)
-
+data = open_data() 
+combo_project.configure(values = data['projects'])
+combo_sonata.configure(values = data['versions'])
+convert_data(data['current_settings'][0],data['current_settings'][1],data['current_settings'][2])
 
 user_lbl.grid(row=1, column=0, ipadx=2, ipady=6, padx=2, pady=5)
 user_ent.grid(row=1, column=1, ipadx=2, ipady=6, padx=2, pady=5)
