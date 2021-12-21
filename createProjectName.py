34e0a326f7835092bfa6be9a55728e1658da635e
Nexzis <36156471+Nexzis@users.noreply.github.com>
2021-12-21 22:13:08 +0300

    changed packing method
    added bg transparency

--- createProjectName.py
+++ createProjectName.py
@@ -9,20 +9,23 @@ import pickle
 now = datetime.datetime.now()
 time = now.strftime("%Y-%m-%d_%H-%M") #Текущая дата и время с использованием strftime:
 app = tkinter.Tk()
-app.geometry('400x150')
+app.geometry("335x100")
 app.resizable(False, False)
-app.title("createProjectName")  
-user_lbl = tkinter.Label(width="7",  text='user')
-combo_project = ttk.Combobox(width="15")
-combo_sonata = ttk.Combobox(width="7")
+app.title("createProjectName")
+app.wm_attributes("-alpha", 0.9)
+user_lbl = tkinter.Label(width="7", text="User")
+project_lbl = tkinter.Label(width="20", text="Projects")
+version_lbl = tkinter.Label(width="15", text="Version")
+combo_project = ttk.Combobox(width="20", height = "5")
+combo_sonata = ttk.Combobox(width="7", height = "5")
 user_ent = Entry(width="7")
-but = Button(text="Gen" , command= lambda:create_project_name( user_ent.get() , combo_project.get() , combo_sonata.get()) )
+but = Button(width="5", height = "1", text="Gen" , command= lambda:create_project_name( user_ent.get() , combo_project.get() , combo_sonata.get()) )
 res = Entry(width="40")
 
 
 data_old = {
-    'user': ['NRS'],
-    'projects': [   "SEV",
+    "user": ["NRS"],
+    "projects": [   "SEV",
                     "KOV_ASU_TP_ESN",
                     "KNG_PHG_VRK",
                     "NOVOUB_ASUE",
@@ -31,20 +34,20 @@ data_old = {
                     "ZPLR_PRU",
                     "ZPLR_SAU_BPG",
                     "SOSN",],
-    'versions': [   "10021",
+    "versions": [   "10021",
                     "10223",
                     "7861",
                     "10255",
                     "10277",
                     "10301",
                     "10335"],
-    'current_settings' : ['test',0,0]
+    "current_settings" : ["test",0,0]
 }
 
 
 def open_data():
     try:
-        with open('config.pickle', 'rb') as f:
+        with open("config.pickle", "rb") as f:
             data = pickle.load(f)
     except FileNotFoundError:
         data = data_old                       
@@ -58,31 +61,33 @@ def convert_data(user_backup,project_backup,sonata_backup):
 
 def create_project_name(user, Project_attr, Sonata_attr):
     res.delete(0, END)
-    res.insert(0,  (user + '_' + Project_attr + '_' + time + '_r' + Sonata_attr))
-    result = user + '_' + Project_attr + '_' + time + '_r' + Sonata_attr
+    res.insert(0,  (user + "_" + Project_attr + "_" + time + "_r" + Sonata_attr))
+    result = user + "_" + Project_attr + "_" + time + "_r" + Sonata_attr
     print(result)
     prepare_data()
-    cmd='echo '+result.strip()+'|clip'
+    cmd="echo "+result.strip()+"|clip"
     return subprocess.check_call(cmd, shell=True)
 
 
 def prepare_data():
-    data['current_settings'][0] = user_ent.get()
-    data['current_settings'][1] = combo_project.current()
-    data['current_settings'][2] = combo_sonata.current()
-    with open('config.pickle', 'wb') as f:
+    data["current_settings"][0] = user_ent.get()
+    data["current_settings"][1] = combo_project.current()
+    data["current_settings"][2] = combo_sonata.current()
+    with open("config.pickle", "wb") as f:
         pickle.dump(data, f) 
 
 
 data = open_data() 
-combo_project.configure(values = data['projects'])
-combo_sonata.configure(values = data['versions'])
-convert_data(data['current_settings'][0],data['current_settings'][1],data['current_settings'][2])
+combo_project.configure(values = data["projects"])
+combo_sonata.configure(values = data["versions"])
+convert_data(data["current_settings"][0],data["current_settings"][1],data["current_settings"][2])
 
-user_lbl.grid(row=1, column=0, ipadx=2, ipady=6, padx=2, pady=5)
-user_ent.grid(row=1, column=1, ipadx=2, ipady=6, padx=2, pady=5)
-combo_project.grid(row=2, column=0, ipadx=2, ipady=6, padx=2, pady=5)
-combo_sonata.grid(row=2, column=1, ipadx=2, ipady=6, padx=2, pady=5)
-but.grid(row=3, column=0, ipadx=2, ipady=6, padx=2, pady=5)
-res.grid(row=3, column=1, ipadx=2, ipady=6, padx=2, pady=5)
+user_lbl.place(x=10, y=10)
+project_lbl.place(x=75, y=10)
+version_lbl.place(x=205, y=10)
+user_ent.place(x=15, y=30)
+combo_project.place(x=75, y=30)
+combo_sonata.place(x=230, y=30)
+but.place(x=15, y=60)
+res.place(x=75, y=63)
 app.mainloop()
