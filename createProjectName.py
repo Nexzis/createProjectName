c91a6fda3f9ce2ad50c13c577d10eb9d7614138e
Nexzis <36156471+Nexzis@users.noreply.github.com>
2023-04-18 16:21:16 +0300

    rebuild app with listboxes
    removed comboboxes, added listboxes, buttons (add,delete), changed save logic to txt file instead of pickle

--- createProjectName.py
+++ createProjectName.py
@@ -1,128 +1,144 @@
-import tkinter
 from tkinter import *
-from tkinter import ttk
 import datetime
 import subprocess
-import pickle
-import easygui
 
+selected_box1 = ''
+selected_box2 = ''
 
-now = datetime.datetime.now()
-time = now.strftime("%Y-%m-%d_%H-%M") #Текущая дата и время с использованием strftime:
-app = tkinter.Tk()
-app.geometry("335x100")
-app.resizable(False, False)
-app.title("createProjectName")
-app.wm_attributes("-alpha", 0.9)
-user_lbl = tkinter.Label(width="7", text="User")
-project_lbl = tkinter.Label(width="20", text="Projects")
-version_lbl = tkinter.Label(width="15", text="Version")
-combo_project = ttk.Combobox(width="20", height = "5")
-combo_sonata = ttk.Combobox(width="7", height = "5")
-user_ent = Entry(width="7")
-but = Button(width="5", height = "1", text="Gen" , command= lambda:create_project_name( user_ent.get() , combo_project.get() , combo_sonata.get()) )
-res = Entry(width="40")
+def on_select_box1(event):
+    global selected_box1
+    selection = event.widget.curselection()
+    if selection:
+        selected_box1 = event.widget.get(selection[0])
 
+def on_select_box2(event):
+    global selected_box2
+    selection = event.widget.curselection()
+    if selection:
+        selected_box2 = event.widget.get(selection[0])
+    
+def load_list(filename,user,box1,box2):
+    with open(filename, 'r') as f:
+        data = f.read().splitlines()
+        
+        user = data[0]
+        index = data.index('----------')
+        items1 = data[2:index]
+        items2 = data[index+1:]
 
-data_old = {
-    "user": ["NRS"],
-    "projects": [   "SEV",
-                    "KOV_ASU_TP_ESN",
-                    "KNG_PHG_VRK",
-                    "NOVOUB_ASUE",
-                    "NOVOURD_ASUE",
-                    "ZPLR_ASU_TP_ESN",
-                    "SOSN",
-                    "Add new project"],
-    "versions": [   "10021",
-                    "10223",
-                    "7861",
-                    "10255",
-                    "10277",
-                    "10301",
-                    "10335",
-                    "Add new version"],
-    "current_settings" : ["test",0,0]
-}
+        entry_user.delete(0, END)
+        box1.delete(0, END)
+        box2.delete(0, END)
+        
+        entry_user.insert(0, user)
+         
+        for item in items1:
+            box1.insert(END, item)
+            
+        for item in items2:
+            box2.insert(END, item)
+            
+def add_item(box, entry):
+    if entry.get() != "":
+        box.insert(END, entry.get())
+        entry.delete(0, END)
 
+def del_list(box):
+    select = list(box.curselection())
+    select.reverse()
+    for i in select:
+        box.delete(i)
+        
+def save_list(user,box1, box2, filename):
+    f = open(filename, 'w')   # Открываем файл в режиме записи
+    f.truncate(0)    # Удаляем предыдущее содержимое файла
+    #user = entry_user.get() 
+    items1 = [f"{item}" for item in box1.get(0, END)]
+    items2 = [f"{item}" for item in box2.get(0, END)]
+    f.write("\n".join([user] + ['*******'] + items1 + ['----------'] + items2))
+    f.close()
 
-def open_data():
-    try:
-        with open("config.pickle", "rb") as f:
-            data = pickle.load(f)
-    except FileNotFoundError:
-        data = data_old                       
-    return data
-
-def convert_data(user_backup,project_backup,sonata_backup):
-    user_ent.insert( 1, user_backup) 
-    combo_project.current(project_backup)   
-    combo_sonata.current(sonata_backup)    
-
-
-def create_project_name(user, Project_attr, Sonata_attr):
+def create_project_name(user, selected_box1, selected_box2):
+    now = datetime.datetime.now()
+    time = now.strftime("%Y-%m-%d_%H-%M") #Текущая дата и время с использованием strftime:
     res.delete(0, END)
-    res.insert(0,  (user + "_" + Project_attr + "_" + time + "_r" + Sonata_attr))
-    result = user + "_" + Project_attr + "_" + time + "_r" + Sonata_attr
+    if not selected_box1 or not selected_box2 or not user:
+        res.insert(0, "Не выбран пользователь, проект или версия")
+        return
+    res.insert(0,  (user + "_" + selected_box1 + "_" + time + "_r" + selected_box2))
+    result = user + "_" + selected_box1 + "_" + time + "_r" + selected_box2
     print(result)
-    prepare_data()
     cmd="echo "+result.strip()+"|clip"
     return subprocess.check_call(cmd, shell=True)
 
-
-def prepare_data():
-    data["current_settings"][0] = user_ent.get()
-    data["current_settings"][1] = combo_project.current()
-    data["current_settings"][2] = combo_sonata.current()
-    with open("config.pickle", "wb") as f:
-        pickle.dump(data, f) 
+root = Tk()
+root.geometry("500x240")
+root.resizable(False, False)
+root.wm_attributes("-alpha", 0.93)
+root.title("createProjectName")
 
 
-def add_new_project(event):
-    res.delete(0, END)
-    if combo_project.get() == 'Add new project':
-        new_project = easygui.enterbox("Add new project")
-    else:
-        pass
-    try: 
-        res.insert(0,  ("new project is " + new_project))
-        data["projects"].append(new_project)
-        data["projects"].sort(key='Add new project'.__eq__)
-    except UnboundLocalError:
-        pass        
-    combo_project.configure(values = data["projects"])
+# общий фрейм
+Button(root, text="Save config", command=lambda: save_list(entry_user.get(), box1, box2, 'list.txt'), width="9")\
+    .pack(side=TOP,anchor=W)
+frame1 = Frame(root)
+frame1.pack(side=TOP, padx=5,anchor=N)
+frame_user = Frame(frame1)
+frame_user.pack(side=LEFT, padx=5,anchor=N)
+frame_prj = Frame(frame1)
+frame_prj.pack(side=LEFT, padx=5,anchor=N)
+frame_ver = Frame(frame1)
+frame_ver.pack(side=LEFT, padx=5,anchor=N)
+frame_res = Frame(root)
+frame_res.pack(side=TOP, padx=5,anchor=N,fill=X,)
 
+# набор для пользователя
 
-def add_new_version(event):
-    res.delete(0, END)
-    if combo_sonata.get() == 'Add new version':
-        new_version = easygui.enterbox("Add new version")
-    else:
-        pass
-    try:
-        res.insert(0,  ("new version is " + new_version))
-        data["versions"].append(new_version)
-        data["versions"].sort()
-        data["versions"].sort(key='Add new version'.__eq__)
-    except UnboundLocalError:
-        pass
-    combo_sonata.configure(values = data["versions"])
+lbl_user = Label(frame_user,width="7", text="User")
+lbl_user.pack(anchor=N)
+entry_user = Entry(frame_user , width="7")
+entry_user.pack(anchor=N)
 
+# набор для проекта
+lbl_prj = Label(frame_prj,width="20", text="Projects")
+lbl_prj.pack(side=TOP, padx=5)
+box1 = Listbox(frame_prj, selectmode=EXTENDED)
+box1.pack(side=LEFT)
+box1.bind('<<ListboxSelect>>', on_select_box1)
+scroll1 = Scrollbar(frame_prj, command=box1.yview)
+scroll1.pack(side=LEFT, fill=Y)
+box1.config(yscrollcommand=scroll1.set)
 
-data = open_data() 
-combo_project.configure(values = data["projects"])
-combo_sonata.configure(values = data["versions"])
-convert_data(data["current_settings"][0],data["current_settings"][1],data["current_settings"][2])
-combo_project.bind("<<ComboboxSelected>>", add_new_project)
-combo_sonata.bind("<<ComboboxSelected>>", add_new_version)
+entry1 = Entry(frame_prj,width="12")
+entry1.pack()
+Button(frame_prj, text="Add", command=lambda: add_item(box1, entry1),width="9")\
+    .pack()
+Button(frame_prj, text="Delete", command=lambda: del_list(box1),width="9")\
+    .pack()
 
+# набор для версии
+lbl_ver = Label(frame_ver,width="15", text="Version")
+lbl_ver.pack(side=TOP, padx=5)
+box2 = Listbox(frame_ver, selectmode=EXTENDED ,width="12")
+box2.pack(side=LEFT)
+box2.bind('<<ListboxSelect>>', on_select_box2)
+scroll2 = Scrollbar(frame_ver, command=box2.yview)
+scroll2.pack(side=LEFT, fill=Y)
+box2.config(yscrollcommand=scroll2.set)
 
-user_lbl.place(x=10, y=10)
-project_lbl.place(x=75, y=10)
-version_lbl.place(x=205, y=10)
-user_ent.place(x=15, y=30)
-combo_project.place(x=75, y=30)
-combo_sonata.place(x=230, y=30)
-but.place(x=15, y=60)
-res.place(x=75, y=63)
-app.mainloop()
+entry2 = Entry(frame_ver,width="12")
+entry2.pack()
+Button(frame_ver, text="Add", command=lambda: add_item(box2, entry2),width="9")\
+    .pack()
+Button(frame_ver, text="Delete", command=lambda: del_list(box2),width="9")\
+    .pack()
+
+# Набор результата
+but = Button(frame_res, width="5", height = "1", text="Gen" , command= lambda:create_project_name( entry_user.get() , selected_box1 , selected_box2) )
+res = Entry(frame_res,width="80")
+but.pack(side=LEFT)
+res.pack(side=RIGHT, padx=5 ,fill=X)
+
+load_list('list.txt', entry_user, box1, box2)
+
+root.mainloop()
